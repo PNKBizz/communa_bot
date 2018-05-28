7f0ab40b5eb38c0cf56f3380e2c79212c0586a92
Pavel Galkin <bizz.gc@gmail.com>
2018-05-28 07:04:26 +0300

    bodyParser

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const telegraf = require('telegraf');
 
 const app = express();
+app.use(express.json());
 
 app.post('/sendMessage', (req, res) => {
   const bot = new telegraf.Telegram(process.env.BOT_ID);
