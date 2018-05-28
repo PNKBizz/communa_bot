185db715b7d4de0b7acb40fbb8cee660d9564749
Pavel Galkin <bizz.gc@gmail.com>
2018-05-28 06:38:42 +0300

    configurable

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ const telegraf = require('telegraf');
 
 const app = express();
 
-app.get('*', () => {
+app.post('/sendMessage', (req) => {
   const bot = new telegraf.Telegram(process.env.BOT_ID);
-  bot.sendMessage(process.env.CHAT_ID, 'New user joined');
+  const { text } = req.body;
+  bot.sendMessage(process.env.CHAT_ID, text);
 });
 
 app.listen(process.env.PORT || 8080);
