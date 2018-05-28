a89144ace5586654f20c939c88ea832aa1a25436
Pavel Galkin <bizz.gc@gmail.com>
2018-05-28 06:35:49 +0300

    listen

--- server.js
+++ server.js
@@ -7,3 +7,5 @@ app.get('*', () => {
   const bot = new telegraf.Telegram(process.env.BOT_ID);
   bot.sendMessage(process.env.CHAT_ID, 'New user joined');
 });
+
+app.listen(process.env.PORT || 8080);
