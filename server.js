f304b62b3bc90edce0ffe9935240cc26d3e1d8ac
Pavel Galkin <bizz.gc@gmail.com>
2018-05-28 06:45:57 +0300

    log

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ const telegraf = require('telegraf');
 
 const app = express();
 
-app.post('/sendMessage', (req) => {
+app.post('/sendMessage', (req, res) => {
   const bot = new telegraf.Telegram(process.env.BOT_ID);
   const { text } = req.body;
+  console.log(text);
   bot.sendMessage(process.env.CHAT_ID, text);
+  res.send('ok');
 });
 
 app.listen(process.env.PORT || 8080);
